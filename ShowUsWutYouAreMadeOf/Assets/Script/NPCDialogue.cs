f4cb9298c3e5f52788134327b844a2d4876d852c
Kelly Liang <kyqliang@gmail.com>
2022-09-03 13:40:25 +0200

    created assembly reference and starting to reorganize dialogue

---
+++ ShowUsWutYouAreMadeOf/Assets/Script/NPCDialogue.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class Dialogue
+{
+
+}
+public class NPCDialogue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
