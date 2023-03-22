b71337e3ca99a3ad7f8d7bfb8379746884825822
Kelly Liang <kyqliang@gmail.com>
2023-03-22 11:42:38 +0000

    change skin eye hair color button

---
+++ ShowUsWutYouAreMadeOf/Assets/Script/Wardrobe/MaterialColorChangeBtn.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class MaterialColorChangeBtn : MonoBehaviour
+{
+    //get the material name based on the type
+    //onclick change to this color
+
+    enum MaterialType {Hair, Skin, Eye}
+    [SerializeField] MaterialType materialType;
+    [SerializeField] Material newMaterial;
+    Transform player;
+    SkinnedMeshRenderer targetRenderer;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ChangeMaterialColor);
+        player = GameManager.Instance.playerObject.transform;
+        FindMaterial();
+        
+    }
+
+    void FindMaterial()
+    {
+        switch(materialType)
+        {
+            case MaterialType.Hair:
+                targetRenderer = player.Find("hair").GetComponent<SkinnedMeshRenderer>();
+                return;
+            case MaterialType.Skin:
+                targetRenderer = player.Find("body").GetComponent<SkinnedMeshRenderer>();
+                return;
+            case MaterialType.Eye:
+                targetRenderer = player.Find("eye").GetComponent<SkinnedMeshRenderer>();
+                return;
+        }
+    }
+
+    void ChangeMaterialColor()
+    {
+        switch(materialType)
+        {
+            case MaterialType.Hair:
+                targetRenderer.sharedMaterial = newMaterial;
+                //change the color of the icons too??
+                return;
+            case MaterialType.Skin:
+                targetRenderer.material = newMaterial;
+                return;
+            case MaterialType.Eye:
+                targetRenderer.material = newMaterial;
+                return;
+        }
+    }
+}
